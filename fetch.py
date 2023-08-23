a5c9540f309b567360c2a73159a6c23d472bfffe
MikeKata13 <michaliskatarachias@gmail.com>
2023-08-23 16:51:13 +0200

    Fetching papers and saving relevant information.

--- fetch.py
+++ fetch.py
@@ -1,13 +1,22 @@
 import arxiv
 
 search = arxiv.Search(
-    query="climate change",
-    max_results=10,
-    sort_by=arxiv.SortCriterion.SubmittedDate
+    query="climate",
+    max_results=2,
+    sort_by=arxiv.SortCriterion.SubmittedDate,
+    sort_order=arxiv.SortOrder.Descending
 )
 
-for result in search.results():
-    print(f"Title: ",result.title)
-    print(f"Authors",result.authors)
-    print(f"Summary: ",result.summary)
-    print("\n")
+with open("results.txt", "w", encoding="utf-8") as file:
+    for result in search.results():
+        result.download_pdf()
+        authors = ", ".join([author.name for author in result.authors])
+        file.write(f"ID: {result.entry_id}\n")
+        file.write(f"Published: {result.published}\n")
+        file.write(f"DOI: {result.doi}\n")
+        file.write(f"Title: {result.title}\n")
+        file.write(f"Authors: {authors}\n")
+        file.write(f"Summary: {result.summary}\n")
+        file.write("\n")
+
+print("Fetching Completed Successfully!")
